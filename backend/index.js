7b8cc9c7176e7251d2e0e70248f6255cbcc4e667
Your Name <ranamostafa2210@gmail.com>
2024-10-08 21:55:54 +0300

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log("db error", err.message);
+  });
 
 const app = express();
 
