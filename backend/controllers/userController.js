5b8e95208dbbdfdeec853a100344240636108f6e
Your Name <ranamostafa2210@gmail.com>
2024-10-13 01:18:15 +0300

    add delete user api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,3 +38,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You can delete only your account"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User deleted successfully");
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateUser } from "../controllers/userController.js";
+import { deleteUser, updateUser } from "../controllers/userController.js";
 import { verfiyToken } from "../utils/verfiyUser.js";
 
 const router = express.Router();
 
 router.patch("/update/:userId", verfiyToken, updateUser);
+router.delete("/delete/:userId", verfiyToken, deleteUser);
 
 export default router;
